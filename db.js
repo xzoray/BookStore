9a3387edd2caccc1ad0fcc680bd0ca03fa4b6974
Sohrab Arenja <sohrab-arenja8@hotmail.de>
2025-01-16 17:29:25 +0100

    first commit

---
+++ db.js
@@ -0,0 +1,131 @@
+let books = [
+    {
+      "name": "Harry Potter and the Philosopher's Stone",
+      "author": "J.K. Rowling",
+      "likes": 2500,
+      "liked": true,
+      "price": 20.00,
+      "publishedYear": 1997,
+      "genre": "Fantasy",
+      "comments": [
+        {
+          "name": "MagicFan",
+          "comment": "Ein magischer Start in eine wundervolle Serie!"
+        },
+        {
+          "name": "BookLover123",
+          "comment": "Hat meine Liebe zur Fantasy neu entfacht."
+        }
+      ]
+    },
+    {
+      "name": "Harry Potter and the Chamber of Secrets",
+      "author": "J.K. Rowling",
+      "likes": 2300,
+      "liked": false,
+      "price": 22.50,
+      "publishedYear": 1998,
+      "genre": "Fantasy",
+      "comments": [
+        {
+          "name": "MysterySeeker",
+          "comment": "Die Geheimnisse der Kammer waren unglaublich spannend."
+        },
+        {
+          "name": "WitchReader",
+          "comment": "Ein faszinierendes Abenteuer mit dunklen Wendungen."
+        }
+      ]
+    },
+    {
+      "name": "Harry Potter and the Prisoner of Azkaban",
+      "author": "J.K. Rowling",
+      "likes": 3000,
+      "liked": true,
+      "price": 23.00,
+      "publishedYear": 1999,
+      "genre": "Fantasy",
+      "comments": [
+        {
+          "name": "TimeTurnerFan",
+          "comment": "Die Zeitreise-Elemente haben die Geschichte auf ein neues Level gebracht."
+        },
+        {
+          "name": "LupinLover",
+          "comment": "Professor Lupin ist mein Lieblingscharakter!"
+        }
+      ]
+    },
+    {
+      "name": "Harry Potter and the Goblet of Fire",
+      "author": "J.K. Rowling",
+      "likes": 3200,
+      "liked": true,
+      "price": 25.00,
+      "publishedYear": 2000,
+      "genre": "Fantasy",
+      "comments": [
+        {
+          "name": "TournamentFan",
+          "comment": "Das Trimagische Turnier war atemberaubend!"
+        }
+      ]
+    },
+    {
+      "name": "Harry Potter and the Order of the Phoenix",
+      "author": "J.K. Rowling",
+      "likes": 2800,
+      "liked": false,
+      "price": 24.00,
+      "publishedYear": 2003,
+      "genre": "Fantasy",
+      "comments": [
+        {
+          "name": "SiriusFan",
+          "comment": "Die emotionale Tiefe dieser Geschichte hat mich umgehauen."
+        },
+        {
+          "name": "DumbledoresArmy",
+          "comment": "Ich wollte Teil von Dumbledores Armee sein!"
+        }
+      ]
+    },
+    {
+      "name": "Harry Potter and the Half-Blood Prince",
+      "author": "J.K. Rowling",
+      "likes": 3500,
+      "liked": true,
+      "price": 26.00,
+      "publishedYear": 2005,
+      "genre": "Fantasy",
+      "comments": [
+        {
+          "name": "PotionsFan",
+          "comment": "Die Enthüllung über den Halbblutprinzen war ein Meisterwerk."
+        },
+        {
+          "name": "SnapeLoyalist",
+          "comment": "Snape ist so ein faszinierender Charakter!"
+        }
+      ]
+    },
+    {
+      "name": "Harry Potter and the Deathly Hallows",
+      "author": "J.K. Rowling",
+      "likes": 4000,
+      "liked": false,
+      "price": 30.00,
+      "publishedYear": 2007,
+      "genre": "Fantasy",
+      "comments": [
+        {
+          "name": "FinalBattleFan",
+          "comment": "Der finale Kampf hat mich vollkommen überwältigt."
+        },
+        {
+          "name": "HorcruxHunter",
+          "comment": "Das Ende einer epischen Reise, die ich nie vergessen werde."
+        }
+      ]
+    }
+  ]
